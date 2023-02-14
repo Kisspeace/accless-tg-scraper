950f759caf786e86a8a62355feacd7f88a6e09a4
Kisspeace <kisspeace@github.com>
2023-02-13 19:34:24 -0500

    Minor update.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name = 'accless_tg_scraper',
-    version = '0.1.0',
-    author='Kisspeace',
-    keywords='telegram scraper parser web',
-    url='http://github.com/Kisspeace/accless-tg-scraper',
-    description='Scrap telegram web WITHOUT account or API token',
-    packages = ['accless_tg_scraper', 
+    version = '0.2.0',
+    author = 'Kisspeace',
+    keywords = 'telegram scraper parser web',
+    url = 'http://github.com/Kisspeace/accless-tg-scraper',
+    description = 'Scrap telegram web WITHOUT account or API token',
+    packages = ['accless_tg_scraper',
                 'accless_tg_scraper.serialize'],
-    install_requires=[
+    install_requires = [
         'aiohttp',
         'bs4'
     ]
